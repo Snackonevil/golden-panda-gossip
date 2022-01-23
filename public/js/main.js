4125d4d2b727fb6e5010795f573e6d3bb1dcf28c
Kevin Lacson <lacsonky@gmail.com>
2022-01-23 15:38:46 -0500

    trying to figure out messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,29 @@
 const socket = io();
 
+const chatForm = document.getElementById("chat-form");
+const chatWindow = document.getElementById("chat-window");
+
 socket.on("message", message => {
     outputMsg(message);
+
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 });
 
-$("#chat-form").on("submit", e => {
+chatForm.addEventListener("submit", e => {
     e.preventDefault();
 
     const msg = e.target.elements.msg.value;
 
     socket.emit("chat-msg", msg);
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
 });
 
 // Output to DOM
 function outputMsg(msg) {
     const div = document.createElement("div");
     div.classList.add("message");
-    div.innerHTML = `<p>${msg}</p>`;
+    div.innerHTML = `<p>USER <span>3:23pm</span></p>
+    <p>${msg}</p>`;
     document.getElementById("chat-window").appendChild(div);
 }
